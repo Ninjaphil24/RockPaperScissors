c0f7d0e803ddb11ebc8c7b987f4312e56fb3f189
Ninjaphil24 <mphilippos@hotmail.com>
2022-02-10 08:56:15 +0200

    Add input.  Working

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+if (document.readyState == 'loading') {
+	document.addEventListener('DOMContentLoaded', ready)
+} 
+// else {
+// 	ready()
+//   playRound()
+// }
+
+function ready() {
+	var playerSelection = new URL(window.location).searchParams.get('playerSelection')
+	document.getElementById('myInput').value = playerSelection
+  return playerSelection
+	
+}
+
+
 function computerPlay() {
 
 let x = Math.floor((Math.random() * 3) + 1);
@@ -20,20 +36,29 @@ return result;
 }
 
 
-const playerSelection = "RoCk";
+// const playerSelection = "rock";
 const computerSelection = computerPlay();
-
+// Create a function that will send scores to url
+let playerScore = 0;
+let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
     // your code here!
-    if(computerSelection=="paper"&&playerSelection=="rock")return "You Lose! Paper beats Rock";
-    else if(computerSelection=="scissors"&&playerSelection=="paper")return "You Lose! Scissors beats Paper";
-    else if(computerSelection=="rock"&&playerSelection=="scissors")return "You Lose! Rock beats Scissors";
-    else if(computerSelection=="rock"&&playerSelection=="paper")return "You Win! Paper beats Rock";
-    else if(computerSelection=="paper"&&playerSelection=="scissors")return "You Win! Scissors beats Paper";
-    else if(computerSelection=="scissors"&&playerSelection=="rock")return "You Win! Rock beats Scissors";
+    if(computerSelection=="paper"&&playerSelection=="rock"
+    ||computerSelection=="scissors"&&playerSelection=="paper"
+    ||computerSelection=="rock"&&playerSelection=="scissors") {
+        computerScore+=1;
+        return "You Lose! "+ computerSelection.toUpperCase() + " beats " + playerSelection.toUpperCase();}
+    else if(computerSelection=="rock"&&playerSelection=="paper"
+    ||computerSelection=="paper"&&playerSelection=="scissors"
+    ||computerSelection=="scissors"&&playerSelection=="rock") {
+        playerScore++;
+        return "You Win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase();}
     else if(computerSelection==playerSelection)return "Draw!  Play again!";
     else return "You misspelled the word!"
 }
 
 
-document.querySelector("body").innerHTML = playRound(playerSelection.toLowerCase(), computerSelection)
+
+document.getElementById("result").innerHTML = playRound(ready().toLowerCase(),computerSelection);
+document.getElementById("playerScore").innerHTML = "Your Score: " + playerScore;
+document.getElementById("computerScore").innerHTML = "Computer Score: " + computerScore;
